bc929bb693c5d0e65329dccff58d4b2353a45ec4
Gunnar R <gunnarrosenberg@gmail.com>
2020-04-14 15:40:40 -0700

    random urls working

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+//CGR
+
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+
+async function retrieveUrl() {
+  // fetch URL variants
+  const resp = await fetch('https://cfw-takehome.developers.workers.dev/api/variants');
+
+  if (resp.code == 200) {console.log('success')};
+
+  // assign URLs to variant options
+  const {variants: [variant1, variant2]} = await resp.json();
+
+  // generate fairly random (50/50 odds) boolean
+  const selection = Math.random() >= 0.5;
+
+  // return randomly selected URL in A/B manner
+  return (selection ? variant1 : variant2);
+
 }
+
+async function handleRequest(request) {
+  return new Response(await retrieveUrl(), { status: 200 })
+}
+
